d848ebb7107eee2d745b74a2847345cbf5bae873
ZhaoYiwei0311 <130329494+ZhaoYiwei0311@users.noreply.github.com>
2025-03-14 13:36:57 +1000

    Webhook Completed. Pay attention to usage of ngrok

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/protected()", 
+    "/protected(.*)"
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect();
 });
+
+// export default clerkMiddleware();
 
 export const config = {
   matcher: [
