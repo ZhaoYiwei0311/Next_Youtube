ecc4c21651cd930ac36d5f491ac64a5f7fc902b4
ZhaoYiwei0311 <130329494+ZhaoYiwei0311@users.noreply.github.com>
2025-03-17 23:08:22 +1000

    Studio videos completed

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,8 +1,16 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/views/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
 const Page = () => {
+    void trpc.studio.getMany.prefetchInfinite({
+        limit: DEFAULT_LIMIT
+    });
+
     return (
-        <div>
-            Studio
-        </div>
+        <HydrateClient>
+            <StudioView />
+        </HydrateClient>
     )
 };
 
