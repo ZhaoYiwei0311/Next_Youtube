b5234d5fd5cebbe8dc75dc004e5f1170178fd66e
ZhaoYiwei0311 <130329494+ZhaoYiwei0311@users.noreply.github.com>
2025-03-21 10:47:14 +1000

    Mux integration completed

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -13,8 +13,12 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { format } from "date-fns";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideoSection = () => {
     return (
@@ -58,16 +62,40 @@ const VideosSectionSuspense = () => {
                         <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                             <TableRow onClick={() => router.push(`/studio/videos/${video.id}`)} className="cursor-pointer">
                                 <TableCell>
-                                    {video.title}
+                                    <div className="flex items-center gap-4">
+                                        <div className="relative aspect-video w-36 shrink-0">
+                                            <VideoThumbnail 
+                                                title={video.title}
+                                                imageUrl={video.thumbnailUrl} 
+                                                previewUrl={video.previewUrl}
+                                                duration={video.duration || 0}
+                                            />
+                                        </div>
+                                        <div className="flex flex-col overflow-hidden gap-1">
+                                            <span className="text-sm line-clamp-1">{video.title}</span>
+                                            <span className="text-xs text-muted-foreground line-clamp-1">
+                                                {video.description || "No descripton"}
+                                            </span>
+                                        </div>
+                                    </div>
                                 </TableCell>
                                 <TableCell>
-                                    {video.title}
+                                    <div className="flex items-center">
+                                        {video.visibility === "private" ? (
+                                            <LockIcon className="size-4 mr-2" />
+                                        ): (
+                                            <Globe2Icon className="size-4 mr-2" />
+                                        )}
+                                        {snakeCaseToTitle(video.visibility)}
+                                    </div>
                                 </TableCell>                                
                                 <TableCell>
-                                    {video.title}
+                                    <div className="flex items-center">
+                                        {snakeCaseToTitle(video.muxStatus || "error")}
+                                    </div>
                                 </TableCell>                                
-                                <TableCell>
-                                    {video.title}
+                                <TableCell className="text-sm truncate">
+                                    {format(new Date(video.createdAt), "d MMM yyyy")}
                                 </TableCell>                                
                                 <TableCell>
                                     {video.title}
@@ -93,17 +121,3 @@ const VideosSectionSuspense = () => {
         </div>
     )
 }
-
-// export const VideoSection = () => {
-//     const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-//         limit: DEFAULT_LIMIT
-//     }, {
-//         getNextPageParam: (lastPage) => lastPage.nextCursor,
-//     });
-
-//     return (
-//         <div>
-//             {JSON.stringify(data)}
-//         </div>
-//     )
-// }
