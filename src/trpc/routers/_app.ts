f6eaa90c8a221eeaabc5526ab134bda0584aa233
ZhaoYiwei0311 <130329494+ZhaoYiwei0311@users.noreply.github.com>
2025-04-01 23:07:59 +1000

    Video reactions completeed

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { categoriesRouter } from '@/modules/categories/server/procedure';
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedute';
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedute';
 
 export const appRouter = createTRPCRouter({
     studio: studioRouter,
     categories: categoriesRouter,
     videos: videosRouter,
-    videoViews: videoViewsRouter
+    videoViews: videoViewsRouter,
+    videoReactions: videoReactionsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
